a831109ac953d92d126eeaf58bfc836d2863563d
Paul Baumgart <paul@proxv.com>
2015-02-04 13:00:46 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var redis = require('redis');
+var timers = require('timers');
+
+function RedisClientManager(redisConfig) {
+  redisConfig = redisConfig || {};
+  this._redisDbNum = redisConfig.database || redisConfig.db || redisConfig.dbNum || 0;
+  this._redisConfig = redisConfig;
+  this._redisClientsByName = {};
+}
+
+RedisClientManager.prototype.getClient = function(name, cb) {
+  var redisClient = this._redisClientsByName[name] || redis.createClient(this._redisConfig);
+  this._redisClientsByName[name] = redisClient;
+  if (this._redisDbNum) {
+    redisClient.select(this._redisDbNum, function(err) {
+      if (err) {
+        cb(err);
+      } else {
+        cb(null, redisClient);
+      }
+    });
+  } else {
+    timers.setImmediate(cb, null, redisClient);
+  }
+};
+
+module.exports = RedisClientManager;
